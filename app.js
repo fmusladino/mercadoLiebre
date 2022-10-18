a9fa70c2ba93e615b01d4d06d39010059e583043
fmusladino <fmusladino@hotmail.com>
2022-10-18 20:19:17 -0300

    vamosdenuevo

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ app.use(express.static('public'));
 const path = require('path');
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando en puerto 3000');
-});
+const port = process.env.PORT || 3001;
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`));
+
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + ('/views/home.html'));
